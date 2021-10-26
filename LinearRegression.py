2db0e2f77715c6addcf60784590d32bd65cabdde
gstei090 <gstei090@uottawa.ca>
2021-10-26 14:52:25 -0700

    basic linear regression model

---
+++ LinearRegression.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Data Generation
+input_data = np.random.randint(1,10,(1000,1))
+output_data = 2*np.array(input_data)
+
+#Data Processing
+X = torch.from_numpy(input_data.astype(np.float32))
+Y = torch.from_numpy(output_data.astype(np.float32))
+y = Y.view(Y.shape[0], 1)
+
+#Model Definition
+sample_size, input_size = X.shape
+output_size = 1
+model = nn.Linear(input_size, output_size)
+
+# Loss and Optimizer Definition
+learning_rate = 0.001
+criterion = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  
+
+# Training
+num_epochs = 100
+for epoch in range(num_epochs):
+    # Forward pass and loss
+    y_predicted = model(X)
+    loss = criterion(y_predicted, y)
+    
+    # Backward pass and update
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if (epoch+1) % 5 == 0:
+        print(f'epoch: {epoch+1}, loss = {loss.item():.4f}')
+        # Plotting
+        predicted = model(X).detach().numpy()
+
+        plt.plot(X, Y, 'ro')
+        plt.plot(X, predicted, 'b')
+
+#Show final graph
+plt.show()

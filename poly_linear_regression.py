f5ddd98d6fbce0a9756153219c00f6919d93c8c3
gstei090 <gstei090@uottawa.ca>
2021-10-27 17:37:41 -0700

    added device configuration to enable cuda

--- poly_linear_regression.py
+++ poly_linear_regression.py
@@ -3,33 +3,34 @@ import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class NeuralNet(nn.Module):
     def __init__(self, input_size, output_size, hidden_layer_size):
         super(NeuralNet, self).__init__()
         self.input_size = input_size
-        self.sig = nn.Sigmoid()
-        self.relu = nn.ReLU()
-        self.l1 = nn.Linear(input_size, hidden_layer_size) 
-        self.l2 = nn.Linear(hidden_layer_size, hidden_layer_size)  
-        self.l3 = nn.Linear(hidden_layer_size, hidden_layer_size)  
-        self.l4 = nn.Linear(hidden_layer_size, hidden_layer_size)  
-        self.l5 = nn.Linear(hidden_layer_size, hidden_layer_size)  
-        self.l6 = nn.Linear(hidden_layer_size, output_size)  
+        self.sig = nn.Sigmoid().to(device)
+        self.l1 = nn.Linear(input_size, hidden_layer_size).to(device)
+        self.l2 = nn.Linear(hidden_layer_size, hidden_layer_size).to(device)  
+        self.l3 = nn.Linear(hidden_layer_size, hidden_layer_size).to(device)
+        self.l4 = nn.Linear(hidden_layer_size, hidden_layer_size).to(device)
+        self.l5 = nn.Linear(hidden_layer_size, hidden_layer_size).to(device)
+        self.l6 = nn.Linear(hidden_layer_size, output_size).to(device)
 
 
     def forward(self, x):
-        out = self.l1(x)
-        out = self.sig(out)
-        out = self.l2(out)
-        out = self.sig(out)
-        out = self.l3(out)
-        out = self.sig(out)
-        out = self.l4(out)
-        out = self.sig(out)
-        out = self.l5(out)
-        out = self.sig(out)
-        out = self.l6(out)
+        out = self.l1(x).to(device)
+        out = self.sig(out).to(device)
+        out = self.l2(out).to(device)
+        out = self.sig(out).to(device)
+        out = self.l3(out).to(device)
+        out = self.sig(out).to(device)
+        out = self.l4(out).to(device)
+        out = self.sig(out).to(device)
+        out = self.l5(out).to(device)
+        out = self.sig(out).to(device)
+        out = self.l6(out).to(device)
 
         return out
 
@@ -43,8 +44,10 @@ for i in range(len(output_data)):
     output_data[i][0]=polynomial_func(output_data[i][0])
 
 #Data Processing
-X = torch.from_numpy(input_data.astype(np.float32))
-Y = torch.from_numpy(output_data.astype(np.float32))
+x_cpu = torch.from_numpy(input_data.astype(np.float32))
+X = x_cpu.to(device)
+y_cpu = torch.from_numpy(output_data.astype(np.float32))
+Y = y_cpu.to(device)
 y = Y.view(Y.shape[0], 1)
 
 #Model Definition
@@ -63,7 +66,7 @@ num_epochs = 10000
 alpha = 0
 for epoch in range(num_epochs):
     # Forward pass and loss
-    y_predicted = model(X)
+    y_predicted = model(X).to(device)
     loss = criterion(y_predicted, y)
     
     # Backward pass and update
@@ -75,17 +78,22 @@ for epoch in range(num_epochs):
         print(f'epoch: {epoch+1}, loss = {loss.item():.4f}')
 
         # Plotting the predictions of the model during the learning process
-        predicted = model(X).detach().numpy()
-        plt.plot(X, predicted, 'bo', alpha=alpha)
+        predicted = model(X).detach().cpu().numpy()
+        plt.plot(x_cpu, predicted, 'bo', alpha=alpha)
         alpha += 0.005
 
 #This is to show the final prediction made by the model (Blue stars)
 # and the initial output data to be compared to (Red squares)
-predicted = model(X).detach().numpy()
-plt.plot(X, Y, 'rs')
-plt.plot(X, predicted, 'b*')
+predicted = model(X).detach().cpu().numpy()
+plt.plot(x_cpu, y_cpu, 'rs')
+plt.plot(x_cpu, predicted, 'b*')
 
 #Show the final graph
 plt.show()
 
-
+# TODO
+# 1. met plus de points et fait un split training and testing. genre 80%-20%. 
+# 2. ajoute du bruit poluynomial aux inputs et outputs
+# 3. Essaie ADAM au lieu SGD
+# 4. rapporte tes resultats sur le test.
+# 5. Use CUDA as the device
